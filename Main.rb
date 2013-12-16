6aa0893bbd0ec5aa1ecfe95b823cac59943daf96
Great Ark Studios <ericz2241@gmail.com>
2013-12-16 09:22:20 -0500

    Update Main.rb

--- Main.rb
+++ Main.rb
@@ -1,1 +1,11 @@
-puts "enter your first number"
+puts "Enter your first number"
+first_num = gets.chomp
+puts "Your first number is #{first_num}"
+puts "Enter your second number"
+second_num = gets.chomp
+puts "Your second number is #{second_num}"
+puts "Your numbers are #{first_num} and #{second_num}."
+puts "Calculating remainder..."
+result = first_num%second_num"
+puts "Your remainder is #{result}."
+
