2572e75f9a325230f2d13279ce681936d9aede54
Great Ark Studios <ericz2241@gmail.com>
2013-12-19 12:17:32 -0500

    Update AdvancedCalculator.rb

--- AdvancedCalculator.rb
+++ AdvancedCalculator.rb
@@ -2,7 +2,7 @@ puts "instructions"
 puts"type plus for addition, sub for subtraction,multi for multiplication, div for division, and mod for mod function."
 puts "what operation would you like to do"
 Operatitpe = gets.chomp
-If Operatitpe = "plus"
+if Operatitpe = "plus"
 puts "Type your first number to add"
 firstadmun = gets.chomp
 puts "Your first number is #{firstadmun}"
