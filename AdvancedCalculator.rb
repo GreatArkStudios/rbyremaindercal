1f67bc3369b4863711c4ff80303cdfd2619ddb7b
Great Ark Studios <ericz2241@gmail.com>
2013-12-22 20:48:26 -0500

    Large and useful update

--- AdvancedCalculator.rb
+++ AdvancedCalculator.rb
@@ -7,4 +7,74 @@ puts "Type your first number to add"
 firstadmun = gets.chomp
 puts "Your first number is #{firstadmun}"
 puts "type a second number"
-
+secondadmun = gets.chomp 
+puts "Your second number is #{secondadmun}"
+puts "The calculator will now add #{firstadmun} and #{secondadmun}"
+addresult=firstadmun+secondadmun 
+puts "Your result  is #{addresult}"
+puts "Would you like to do another calculation" 
+turnpoint1 = gets.chomp
+if turnpoint1 == "yes"
+puts "type plus for addition, sub for subtraction,multi for multiplication, div for division, and mod for mod function."
+puts "what operation would you like to do"
+if Operatitpe = "sub"
+puts "Type your first number to add"
+firstadmun = gets.chomp
+puts "Your first number is #{firstadmun}"
+puts "type a second number"
+secondadmun = gets.chomp 
+puts "Your second number is #{secondadmun}"
+puts "The calculator will now add #{firstadmun} and #{secondadmun}"
+addresult=firstadmun-secondadmun 
+puts "Your result  is #{addresult}"
+puts "Would you like to do another calculation" 
+turnpoint1 = gets.chomp
+if turnpoint1 == "yes"
+puts "type plus for addition, sub for subtraction,multi for multiplication, div for division, and mod for mod function."
+puts "what operation would you like to do"
+if Operatitpe = "multi"
+puts "Type your first number to add"
+firstadmun = gets.chomp
+puts "Your first number is #{firstadmun}"
+puts "type a second number"
+secondadmun = gets.chomp 
+puts "Your second number is #{secondadmun}"
+puts "The calculator will now add #{firstadmun} and #{secondadmun}"
+addresult=firstadmun+secondadmun 
+puts "Your result  is #{addresult}"
+puts "Would you like to do another calculation" 
+turnpoint1 = gets.chomp
+if turnpoint1 == "yes"
+puts "type plus for addition, sub for subtraction,multi for multiplication, div for division, and mod for mod function."
+puts "what operation would you like to do"
+if Operatitpe = "div"
+puts "Type your first number to add"
+firstadmun = gets.chomp
+puts "Your first number is #{firstadmun}"
+puts "type a second number"
+secondadmun = gets.chomp 
+puts "Your second number is #{secondadmun}"
+puts "The calculator will now add #{firstadmun} and #{secondadmun}"
+addresult=firstadmun+secondadmun 
+puts "Your result  is #{addresult}"
+puts "Would you like to do another calculation" 
+turnpoint1 = gets.chomp
+if turnpoint1 == "yes"
+puts "type plus for addition, sub for subtraction,multi for multiplication, div for division, and mod for mod function."
+puts "what operation would you like to do
+if Operatitpe = "mod"
+puts "Type your first number to add"
+firstadmun = gets.chomp
+puts "Your first number is #{firstadmun}"
+puts "type a second number"
+secondadmun = gets.chomp 
+puts "Your second number is #{secondadmun}"
+puts "The calculator will now add #{firstadmun} and #{secondadmun}"
+addresult=firstadmun+secondadmun 
+puts "Your result  is #{addresult}"
+puts "Would you like to do another calculation" 
+turnpoint1 = gets.chomp
+if turnpoint1 == "yes"
+puts "type plus for addition, sub for subtraction,multi for multiplication, div for division, and mod for mod function."
+puts "what operation would you like to do"
+end
